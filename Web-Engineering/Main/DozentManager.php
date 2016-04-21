414b0db53b37edc5250da2af5c7edf0e2f2d5d54
MarcoLynnK <marco.kagel@googlemail.com>
2016-04-21 14:56:24 +0200

    Aktualisiert

--- Web-Engineering/Main/DozentManager.php
+++ Web-Engineering/Main/DozentManager.php
@@ -36,7 +36,7 @@ class Dozent extends Manager
         }
         return null;
     }
-echo 'Hello again!';
+
     public function create (Dozent $dozent) {
         try {
             $sql= $this->pdo->prepare ('INSERT INTO User (login, vorname, nachname, hash) VALUES (:login, :vorname , :nachname, :hash)');
