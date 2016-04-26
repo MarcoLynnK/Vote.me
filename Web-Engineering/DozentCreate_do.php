4bcc0666f8ae8cbbe8b970b12fad75341d02a9ce
MarcoLynnK <marco.kagel@googlemail.com>
2016-04-26 15:14:48 +0200

    Neu Dozent_create do

---
+++ Web-Engineering/DozentCreate_do.php
@@ -0,0 +1,31 @@
+<?php include("include/session_check.php"); ?>
+
+<?php
+
+require_once("Main/DozentManager.php");
+require_once("Main/Classes.php");
+
+$login = htmlspecialchars($_POST["login"], ENT_QUOTES, "UTF-8");//htmlspecialchars filtert aus der eingabe die Sonderzeichen bzw. spezielle begriffe raus (Name= DROP ’user’)
+$firstname = htmlspecialchars($_POST["firstname"], ENT_QUOTES, "UTF-8");
+$lastname = htmlspecialchars($_POST["lastname"], ENT_QUOTES, "UTF-8");
+$faculty = htmlspecialchars($_POST["faculty"], ENT_QUOTES, "UTF-8");
+$password = htmlspecialchars($_POST["password"], ENT_QUOTES, "UTF-8");
+
+if (!empty($login) && !empty($firstname) && !empty($lastname) && !empty($faculty) && !empty($password))
+{
+    $userdata =
+        [
+        "login" => $login,
+        "vorname" => $firstname,
+        "nachname" => $lastname,
+        "hash" => password_hash($password, PASSWORD_DEFAULT)
+        ];
+
+    $dozent = new Dozent($userdata);
+    $dozentManager = new DozentManager();
+    $dozentManager->create($dozent);
+    header('Location: index.php');
+}
+else {
+    echo "Bitte alle Felder ausfüllen!<br/>";
+}
