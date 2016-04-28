a6721decb4dccfbfb5a60ff4dbf71b6c5941146f
MarcoLynnK <marco.kagel@googlemail.com>
2016-04-28 15:15:41 +0200

    Alles umstruktureren, Noch nicht fertig...
    Anpassen an DB...

--- Web-Engineering/Login_do.php
+++ Web-Engineering/Login_do.php
@@ -6,7 +6,7 @@ $login = htmlspecialchars($_POST["login"], ENT_QUOTES, "UTF-8");
 $password = htmlspecialchars($_POST["password"], ENT_QUOTES, "UTF-8");
 
 if (!empty($login) && !empty($password)) {
-    $dozentmanagerManager = new DozentManager();
+    $userManager= new UserManager($con);
     $user = $userManager->findByLogin($login, $password);//holt sich den User aus der Datenbank durch Suche nach dem Login
     if ($user==null) {
         //header('Location: login.php');
