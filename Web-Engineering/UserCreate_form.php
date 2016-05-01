2d1be0cf909a96e66a2daaa4a58322db94c4fe7d
ea014 <ea014@hdm-stuttgart.de>
2016-05-01 22:34:56 +0200

    Chance create wird angepasst

--- Web-Engineering/UserCreate_form.php
+++ Web-Engineering/UserCreate_form.php
@@ -1,27 +1,57 @@
 <!DOCTYPE html>
 <html>
 <head>
+
     <?php
     require_once ('include/header.php')
     ?>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Sign up</title>
+
 </head>
+
 <body>
+<div id="navbar">
 
-<h1> Neuen Benutzer anlegen:</h1>
+    <img src="pic/logo2.svg" id="logo">
 
-<form action="UserCreate_do.php" method="post">
-    Benutzername:<br>
-    <input type="text" name="login" id="login" placeholder="Benutzername"><br><br>
-    Vorname:<br>
-    <input type="text" name="firstname" id="firstname" placeholder="Vorname"><br><br>
-    Nachname:<br>
-    <input type="text" name="lastname" id="lastname" placeholder="Nachname"><br><br>
-    Email:<br>
-    <input type="email" name="email" id="email" placeholder="Email"><br><br>
-    Passwort:<br>
-    <input type="password" name="password" id="password" placeholder="Passwort"><br><br>
-    <input type='submit' value="registrieren">
-</form>
+    <div class="dropdown">
+        <button class="dropbtn">MENU</button>
+        <div class="dropdown-content">
+            <a href="#">VOTINGS</a>
+            <a href="#">SETTINGS</a>
+        </div>
+    </div>
+
+    <a href="log-out.html" style="text-decoration: none;">
+        <button class="log-out" name="LogOut">LOG OUT</button>
+    </a>
+
+</div>
+
+<div id="Container">
+
+    <form class="input-container" action="UserCreate_do.php" method="post">
+        <a class="NewUser">NEW USER</a></br></br>
+
+        <input class="inputForm" id="email" name="E-Mail" type="email" placeholder="HdM E-Mail" ></br></br>
+        <input class="inputForm2" id="firstname" name="FirstName" type="text" placeholder="First name">
+        <input class="inputForm3" id="lastname" name="LastName" type="text" placeholder="Last name"></br></br>
+        <input class="inputForm" id="password" name="Password" type="password" placeholder="Password"></br></br>
+        <button class="submit" name="Submit">SIGN UP</button></br></br>
+    </form>
+</div>
+
 
 
 </body>
+
+</html>
