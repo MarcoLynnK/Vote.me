0ae2026d645f8dd1bf6f8bd5644c67c00b84b541
ea014 <ea014@hdm-stuttgart.de>
2016-05-01 22:04:59 +0200

    Offline daten werden upgeloadet

--- Web-Engineering/ChanceCreate_form.php
+++ Web-Engineering/ChanceCreate_form.php
@@ -3,6 +3,10 @@
     <?php
     require_once ('include/header.php')
     ?>
+
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    
 </head>
 <body>
 
