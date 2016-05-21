e132a0fe6662079bb9357e81b50de0e9b5dafd38
ea014 <ea014@hdm-stuttgart.de>
2016-05-21 19:40:58 +0200

    Voting erzeugen

--- Web-Engineering/test.php
+++ Web-Engineering/test.php
@@ -28,9 +28,9 @@
 
 <div>
 
-    <button class="votebtn" name="Submit"><?php echo " " ?></button></br></br>
-    <button class="votebtn" name="Submit"><?php echo " " ?></button></br></br>
-    <button class="votebtn" name="Submit"><?php echo " " ?></button></br></br>
+    <button class="votebtn" name="Submit"><?php echo " " ?></button></br></br></br>
+    <button class="votebtn" name="Submit"><?php echo " " ?></button></br></br></br>
+    <button class="votebtn" name="Submit"><?php echo " " ?></button>
 
 </div>
 
