33194f0f808578244a40ba11971f0a5267735073
MarcoLynnK <marco.kagel@googlemail.com>
2016-05-23 19:26:31 +0200

    Chance Rausgelöscht, weil brauchen wir nicht! erklär ich morgen :D

--- Web-Engineering/UserDelete_do.php
+++ Web-Engineering/UserDelete_do.php
@@ -2,7 +2,7 @@
 
 <?php
 require_once("Main/Classes.php");
-require_once("Main/ChanceManager.php");
+require_once("Main/UserManager.php");
 
 $login = (int)htmlspecialchars($_GET["login"], ENT_QUOTES, "UTF-8");
 $password= (int)htmlspecialchars($_GET["password"], ENT_QUOTES, "UTF-8");
