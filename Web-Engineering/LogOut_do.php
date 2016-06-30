73f0e4d10cec1c620b964a2565fc995d03030825
ea014 <ea014@hdm-stuttgart.de>
2016-06-30 16:02:22 +0200

    Chart Responsive design

--- Web-Engineering/LogOut_do.php
+++ Web-Engineering/LogOut_do.php
@@ -7,7 +7,9 @@ session_destroy();
 <?php include("include/Head.php")?>
 
 <body>
-
+    <div id="navbar">
+    <img src="img/logo2.svg" id="logo">
+    </div>
     <div class="container">
         <h1>Sie haben sich ausgeloggt.</h1>
         <a href='login.php'>zurück zum Login</a>
