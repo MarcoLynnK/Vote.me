df477d748dffa4f6b21086b4a2200ad485491909
ea014 <ea014@hdm-stuttgart.de>
2016-06-30 16:06:19 +0200

    Chart Responsive design

--- Web-Engineering/LogOut_do.php
+++ Web-Engineering/LogOut_do.php
@@ -3,6 +3,10 @@ session_start();
 $_SESSION = array();
 session_destroy();
 ?>
+<head>
+<link type="text/css" rel="stylesheet" href="css/style.css"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+</head>
 
 <body>
     <img src="img/logo.svg" class="logo"></br></br>
