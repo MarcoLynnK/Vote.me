c2c3fd4a2a5456e8e29037e2039a6b92be13abb1
ea014 <ea014@hdm-stuttgart.de>
2016-07-01 18:17:38 +0200

    Tabellen erstellen editieren blabla

--- Web-Engineering/Voting_Index.php
+++ Web-Engineering/Voting_Index.php
@@ -34,7 +34,7 @@ foreach ($list as $voting) {
     echo "<td>$voting->ID_Voting</td>";
     echo "<td>$voting->name_Voting</td>";
     echo "<td>$voting->question_Voting</td>";
-    echo "<td>
+    echo "<td class='edittable'>
               <a href='VotingRead.php?ID_Voting=$voting->ID_Voting' class='btn btn-success btn-xs'><input type='image' class='editicons' src='img/view.svg'></a>&nbsp;
               <a href='VotingUpdate_form.php?ID_Voting=$voting->ID_Voting' class='btn btn-info btn-xs'><input type='image' class='editicons' src='img/edit.svg'></a>&nbsp;
               <a href='VotingDelete_do.php?ID_Voting=$voting->ID_Voting' class='btn btn-info btn-danger btn-xs'><input type='image' class='editicons' src='img/trash.svg'></a>
