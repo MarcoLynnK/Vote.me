2ea8684d56121301790d9462be7edfa0dc73d5b5
ea014 <ea014@hdm-stuttgart.de>
2016-07-01 19:37:19 +0200

    Tabellen erstellen editieren blabla

--- Web-Engineering/VotingUpdate_form.php
+++ Web-Engineering/VotingUpdate_form.php
@@ -18,31 +18,13 @@ $voting = $VotingManager->findById($ID_Voting);
 <body>
 
 
-<div id="navbar">
-
-    <img src="img/logo2.svg" id="logo">
-
-    <div class="dropdown">
-        <button class="dropbtn">MENU</button>
-        <div class="dropdown-content">
-            <a href="#">VOTING LIST</a>
-            <a href="#">CREATE VOTING</a>
-        </div>
-    </div>
-
-    <a href="log-out.html" style="text-decoration: none;">
-        <button class="log-out" name="LogOut">LOG OUT</button>
-    </a>
-
-</div>
-
 <a>Voting # <?php echo ($voting->ID_Voting) ?></a>
 
 <form action='LectureUpdate_do.php' method='post'>
     <input class="inputForm" type='hidden' name='ID_Voting' value='<?php echo ($voting->ID_Voting) ?>' />
-    Thema des Votings:<br>
+    <a class="text">Thema des Votings:</a><br>
     <input class="inputForm" type='text' name='name_Voting' value='<?php echo ($voting->name_Voting) ?>' placeholder="Thema" /><br>
-    Frage:
+    <a class="text">Frage:</a></br>
     <input class="inputForm" type='text' name='question_Voting' value='<?php echo ($voting->question_Voting) ?>' placeholder="Frage" /><br>
     <input class="submit" type='submit' value='aktualisieren' />
 </form>
