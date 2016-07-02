f81087d4cc11504f5ccf354e1281c9fb12ddc74a
MarcoLynnK <marco.kagel@googlemail.com>
2016-07-02 12:49:00 +0200

    02.07.16 Auswertung in Result

---
+++ Web-Engineering/ChanceRead.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once("Main/VotingManager.php");
+require_once("Main/VotingChanceManager.php");
+require_once("Main/LectureManager.php");
+require_once("Main/Classes.php");
+
+$ID_Chance= htmlspecialchars($_GET["ID_Chance"], ENT_QUOTES, "UTF-8");
+$chanceManager= new VotingManager();
+$chance= $chanceManager->findById($ID_Chance);
+
+$chance->ID_Voting= $ID_Voting;
+
+$votingManager= new VotingManager();
+$voting= $votingManager->findById($ID_Voting);
+?>
+
+<!DOCTYPE html>
+<html>
+
+
+<?php include("include/HeadUser.php"); ?>
+
+
+<body>
+<?php
+print_r($chance);
+echo "<h1 class='topic'><a class='bold'>Votingthema:</a> $voting->name_Voting</h1>";
+echo "<h3 class='text2'><a class='bold'>Antwort:</a> $chance->description_Chance</h3>";
+?>
+</body>
+</html>
