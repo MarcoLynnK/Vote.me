209efc6cb54edce737c7e129a8ab41283b6d032c
MarcoLynnK <marco.kagel@googlemail.com>
2016-07-03 00:43:10 +0200

    Anpassungen Lecture

--- Web-Engineering/LectureRead.php
+++ Web-Engineering/LectureRead.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("Main/LectureManager.php");
+require_once("Main/LectureVotingManager.php");
 require_once("Main/UserManager.php");
 require_once("Main/Classes.php");
 
@@ -8,12 +9,23 @@ $ID_Lecture = htmlspecialchars($_GET["ID_Lecture"], ENT_QUOTES, "UTF-8");
 $lectureManager = new LectureManager();
 $lecture= $lectureManager->findById($ID_Lecture);
 
+$lecturevotingManager= new LectureVotingManager();
+$voting= $lecturevotingManager->findAllVotingBylecture($lecture);
+
 $userManager= new UserManager();
 $user= $userManager->findById($lecture->ID_User);
 ?>
 
 <!DOCTYPE html>
 <html>
+<head>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+
+    <script type="text/javascript" src="js/jquery-1.12.3.js"></script>
+    <script type="text/javascript" src="js/Chart.min.js"></script>
+</head>
 
 <?php include("include/NavbarUser.php"); ?>
 
@@ -24,8 +36,19 @@ $user= $userManager->findById($lecture->ID_User);
 echo "<h1 class='topic'><a class='bold'>Vorlesung Nr. $ID_Lecture</h1>";
 echo "<h3 class='text2'><a class='bold'>Lecture:</a> $lecture->name_Lecture</h3>";
 echo "<h3 class='text2'><a class='bold'>Degreecourse:</a> $lecture->degreecourse</h3>";
-echo "<h3 class='text2'><a class='bold'>Created by:</a> $user->firstname $user->lastname</h3>"
+echo "<h3 class='text2'><a class='bold'>Created by:</a> $user->firstname $user->lastname</h3>";
+if (count($voting)>0)
+{
+    $i=1;
+    foreach ($voting as $vorlesungen)
+    {
+        echo "<h3 class='text2'>Voting $i: $vorlesungen->name_Voting</h3>";
+        $i=$i+1;
+    }
+}
 
+echo "<h3 class='text2'><a href='VotingCreate_form.php?ID_Lecture=$lecture->ID_Lecture'</a>CREATE VOTING</h3>";
+echo "<h3 class='text2'><a href='Lecture_index.php'</a>BACK</h3>";
 ?>
 </body>
 </html>
--- Web-Engineering/LectureUpdate_form.php
+++ Web-Engineering/LectureUpdate_form.php
@@ -10,44 +10,24 @@ $lecture = $LectureManager->findById($ID_Lecture);
 
 <!DOCTYPE html>
 <html>
-<?php include("include/NavbarUser.php"); ?>
-
-<link type="text/css" rel="stylesheet" href="css/style.css"/>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
+<head>
+    <?php include("include/NavbarUser.php"); ?>
+    
+</head>
 
 <body>
 
 
-
-<div id="navbar">
-
-    <img src="img/logo2.svg" id="logo">
-
-    <div class="dropdown">
-        <button class="dropbtn">MENU</button>
-        <div class="dropdown-content">
-            <a href="#">VOTING LIST</a>
-            <a href="#">CREATE VOTING</a>
-        </div>
-    </div>
-
-    <a href="log-out.html" style="text-decoration: none;">
-        <button class="log-out" name="LogOut">LOG OUT</button>
-    </a>
-
-</div>
-
 <a>Vorlesung # <?php echo ($lecture->ID_Lecture) ?></a>
 
-    <form action='LectureUpdate_do.php' method='post'>
-        <input class="inputForm" type='hidden' name='ID_Lecture' value='<?php echo ($lecture->ID_Lecture) ?>' />
-        Name der Vorlesung:<br>
-        <input class="inputForm" type='text' name='name_Lecture' value='<?php echo ($lecture->name_Lecture) ?>' placeholder="Vorlesung" /><br>
-        Studiengang:
-        <input class="inputForm" type='text' name='degreecourse' value='<?php echo ($lecture->degreecourse) ?>' placeholder="Studiengang" /><br>
-        <input class="submit" type='submit' value='aktualisieren' />
-    </form>
+<form action='LectureUpdate_do.php' method='post'>
+    <input class="inputForm" type='hidden' name='ID_Lecture' value='<?php echo ($lecture->ID_Lecture) ?>' />
+    Name der Vorlesung:<br>
+    <input class="inputForm" type='text' name='name_Lecture' value='<?php echo ($lecture->name_Lecture) ?>' placeholder="Vorlesung" /><br>
+    Studiengang:
+    <input class="inputForm" type='text' name='degreecourse' value='<?php echo ($lecture->degreecourse) ?>' placeholder="Studiengang" /><br>
+    <input class="submit" type='submit' value='UPDATE' />
+</form>
 
 </body>
 </html>
