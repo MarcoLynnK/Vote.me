d8b30f0e4f6157c3e72cc4a540075e57d9a02d19
ea014 <ea014@hdm-stuttgart.de>
2016-07-03 17:20:01 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/VotingRead.php
+++ Web-Engineering/VotingRead.php
@@ -49,9 +49,10 @@ $lecture= $lectureManager->findById($voting->ID_Lecture);
 ?>
 </table>
 </div></br></br>
-<button class='submit'><a href='LectureRead.php?<? echo"phpID_Lecture=$voting->ID_Lecture"?>'>SHOW LECTURE</a></button></br></br>
-<button class='submit'><a href='ChanceCreateform.php?ID_Voting=$voting->ID_Voting'>CREATE CHANCE</a></button></br></br>";
-<button class='submit'><a href='Voting_Index.php'>BACK</a></button>"
-
+<?php
+echo "<button class='submit'><a href='LectureRead.php?ID_Lecture=$voting->ID_Lecture'>SHOW LECTURE</a></button></br></br>";
+echo "<button class='submit'><a href='ChanceCreateform.php?ID_Voting=$voting->ID_Voting'</a>CREATE CHANCE</button></br></br>";
+echo "<button class='submit'><a href='Voting_Index.php'</a>BACK</button>"
+?>
 </body>
 </html>
