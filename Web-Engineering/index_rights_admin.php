ee74eb9caa97b85eb06aa3973b74f04329a8f287
ea014 <ea014@hdm-stuttgart.de>
2016-07-03 13:44:22 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/index_rights_admin.php
+++ Web-Engineering/index_rights_admin.php
@@ -18,7 +18,7 @@ include ("include/Session_Check.php");*/
 <h1>Sie sind Admin:</h1>
 <button class="submit" href="User_Index.php">User</button><br><br>
 <button class="submit" href="Lecture_Index.php">Vorlesungen</button><br><br>
-<button class="submit" href="Voting_Index.php">Votings</a><br><br>
+<button class="submit" href="Voting_Index.php">Votings</button><br><br>
 <button class="submit" href="Chance_Index.php">Chance</button>
 
 </body>
