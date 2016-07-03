9783ac33f29a2de42f0a1cac8a4f7b476c749746
ea014 <ea014@hdm-stuttgart.de>
2016-07-03 19:35:01 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/VotingRead.php
+++ Web-Engineering/VotingRead.php
@@ -36,7 +36,7 @@ $lecture= $lectureManager->findById($voting->ID_Lecture);
 <table>
 <?php
     echo "<tr><th>Lecture</th><th> $lecture->name_Lecture</th></tr>";
-    echo "<tr><th>Voting No.</th><th> $voting->ID_Voting</th></tr>";
+    echo "<tr><td>Voting No.</td><td> $voting->ID_Voting</td></tr>";
     echo "<tr><td>Topic:</td><td> $voting->name_Voting</td></tr>";
     echo "<tr><td>Question:</td><td> $voting->question_Voting</td></tr>";
 
