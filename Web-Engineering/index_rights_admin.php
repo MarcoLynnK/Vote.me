6be77d34b81479f70d50276cf17a48b139f539b4
ea014 <ea014@hdm-stuttgart.de>
2016-07-03 18:38:12 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/index_rights_admin.php
+++ Web-Engineering/index_rights_admin.php
@@ -15,7 +15,7 @@ include ("Main/Session_Check.php");
 <?php require_once("include/Navbar.php"); ?>
 
 <div class="container">
-<center><a class="topic">You are Admin</a></center><br><br>
+<a class="topic">You are Admin</a><br><br>
 <a href="User_Index.php"><div class='submit'>User</div></a><br><br>
 <a href="Lecture_Index.php"><div class='submit'>Lecture</div></a><<br><br>
 <a href="Voting_Index.php"><div class='submit'>Votings</div></a><<br><br>
--- Web-Engineering/index_rights_user.php
+++ Web-Engineering/index_rights_user.php
@@ -15,7 +15,7 @@ include ("Main/Session_Check.php");
 <?php require_once("include/Navbar.php"); ?>
 
 <div class="container">
-<center><a class="topic">You are Lecturer</a></center><br><br>
+    <a class="topic">You are Lecturer</a><br><br>
     <a href="Lecture_Index.php"><div class='submit'>Lecture</div></a><br><br>
     <a href="Voting_Index.php"><div class='submit'>Votings</div></a><br><br>
     <a href="Chance_Index.php"><div class='submit'>Chance</div></a>
