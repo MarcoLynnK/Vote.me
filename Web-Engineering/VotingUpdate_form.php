d48268fd0a3821f9af209556be08483fd8855ba8
ea014 <ea014@hdm-stuttgart.de>
2016-07-05 00:07:26 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/VotingUpdate_form.php
+++ Web-Engineering/VotingUpdate_form.php
@@ -12,7 +12,12 @@ $voting = $VotingManager->findById($ID_Voting);
 <!DOCTYPE html>
 <html>
 <head>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
+
+    <script type="text/javascript" src="js/jquery-1.12.3.js"></script>
+    <script type="text/javascript" src="js/Chart.min.js"></script>
 </head>
 <body>
 <?php require_once("include/Navbar.php"); ?>
