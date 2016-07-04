9f6994ab758fdfa20034efbb23e22c4a700ee1f1
ea014 <ea014@hdm-stuttgart.de>
2016-07-05 00:57:46 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/VotingUpdate_do.php
+++ Web-Engineering/VotingUpdate_do.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <script type="text/javascript" src="js/jquery-1.12.3.js"></script>
+    <script type="text/javascript" src="js/Chart.min.js"></script>
+</head>
+<body>
+<?php require_once("include/Navbar.php"); ?>
+
 <?php
 include ("Main/Session_Check.php");
 require_once("Main/VotingManager.php");
@@ -26,7 +37,10 @@ if (!empty ($ID_Voting) && !empty($name_Voting) && !empty($question_Voting))
 
 else 
 {
-    echo "Error: Bitte füllen sie alle Felder aus!<br/>";
-}
+    echo "<a class='text2'>Error: Please fill out all fields!</a><br/>";
+};
 
 ?>
+<a href="VotingUpdate_form.php"> <div class="submit">BACK</div></a>
+</body>
+</html>
