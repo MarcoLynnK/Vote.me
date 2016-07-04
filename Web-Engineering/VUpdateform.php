e4f8691fb05aa2ec89d548b170f810ac6b800632
ea014 <ea014@hdm-stuttgart.de>
2016-07-04 23:12:23 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/VUpdateform.php
+++ Web-Engineering/VUpdateform.php
@@ -9,11 +9,11 @@
 <a>Voting Nr.</a>
 
 
-    <input class="inputForm" type='hidden' name='ID_Voting' value='<?php echo ($voting->ID_Voting) ?>' />
+    <input class="inputForm" type='hidden' name='ID_Voting'  />
     <a class="text">Thema des Votings:</a></br>
-    <input class="inputForm" type='text' name='name_Voting' value='<?php echo ($voting->name_Voting) ?>' placeholder="Topic" /><br>
+    <input class="inputForm" type='text' name='name_Voting' placeholder="Topic" /><br>
     <a class="text">Frage:</a></br>
-    <input class="inputForm" type='text' name='question_Voting' value='<?php echo ($voting->question_Voting) ?>' placeholder="Question" /><br>
+    <input class="inputForm" type='text' name='question_Voting'  placeholder="Question" /><br>
     <a href='Voting_Index.php'><div class='submit'>UPDATE</div></a>"
 
 
