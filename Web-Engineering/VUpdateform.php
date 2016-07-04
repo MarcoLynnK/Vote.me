d7e41d7a8d6bb61ae5af79bc7eea0454b5ed011d
ea014 <ea014@hdm-stuttgart.de>
2016-07-04 23:12:40 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/VUpdateform.php
+++ Web-Engineering/VUpdateform.php
@@ -6,16 +6,5 @@
 <body>
 <?php require_once("include/Navbar.php"); ?>
 
-<a>Voting Nr.</a>
-
-
-    <input class="inputForm" type='hidden' name='ID_Voting'  />
-    <a class="text">Thema des Votings:</a></br>
-    <input class="inputForm" type='text' name='name_Voting' placeholder="Topic" /><br>
-    <a class="text">Frage:</a></br>
-    <input class="inputForm" type='text' name='question_Voting'  placeholder="Question" /><br>
-    <a href='Voting_Index.php'><div class='submit'>UPDATE</div></a>"
-
-
 </body>
 </html>
