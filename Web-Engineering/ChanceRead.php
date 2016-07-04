e534dfc1c6b8927b953feb709b8c6da39e36b0e4
ea014 <ea014@hdm-stuttgart.de>
2016-07-05 00:33:37 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/ChanceRead.php
+++ Web-Engineering/ChanceRead.php
@@ -18,7 +18,6 @@ $voting= $votingManager->findById($chance->ID_Voting);
     <link type="text/css" rel="stylesheet" href="css/style.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-
     <script type="text/javascript" src="js/jquery-1.12.3.js"></script>
     <script type="text/javascript" src="js/Chart.min.js"></script>
 </head>
