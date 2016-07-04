a31ccc7c944303558c4c731319e3c518b081064c
ea014 <ea014@hdm-stuttgart.de>
2016-07-05 00:36:30 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/LectureCreate_form.php
+++ Web-Engineering/LectureCreate_form.php
@@ -4,7 +4,6 @@
     <link type="text/css" rel="stylesheet" href="css/style.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-
     <script type="text/javascript" src="js/jquery-1.12.3.js"></script>
     <script type="text/javascript" src="js/Chart.min.js"></script>
 </head>
@@ -16,7 +15,7 @@
 <form action="LectureCreate_do.php" method="post">
     <input class="inputForm" type="text" name="name_Lecture" placeholder="Lecture"><br><br>
     <input class="inputForm" type="text" name="degreecourse" placeholder="Degreecourse"><br><br>
-    <input class="submit" type='submit' value="CREATE LECTURE">
+    <a href="LectureCreate_form.php"> <div class="submit">CREATE LECTURE</div></a>
 </form>
 
 </body>
