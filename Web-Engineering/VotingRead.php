4452d4f80836f6e67da84d00caa9b784cd253d23
ea014 <ea014@hdm-stuttgart.de>
2016-07-05 21:03:33 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/VotingRead.php
+++ Web-Engineering/VotingRead.php
@@ -129,9 +129,9 @@ echo "<a href='Voting_Index.php'><div class='submit'>BACK</div></a>"
 </div>
 </div>
 <a class="Platzhalter"> </a>
-
+<div class="footer">© 2016 by Vote.me GmbH - <a href="mailto:support@vote.me">Contact</a> - <a href="impressum.php">Impressum</a></div>
 </body>
 <footer>
-    <div class="footer">© 2016 by Vote.me GmbH - <a href="mailto:support@vote.me">Contact</a> - <a href="impressum.php">Impressum</a></div>
+    <div>© 2016 by Vote.me GmbH - <a href="mailto:support@vote.me">Contact</a> - <a href="impressum.php">Impressum</a></div>
 </footer>
 </html>
