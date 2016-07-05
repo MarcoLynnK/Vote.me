ab9d11e07c92944646c86984f2d93745f19e0e86
MarcoLynnK <marco.kagel@googlemail.com>
2016-07-05 13:01:24 +0200

    evtl Voting/Chancemanager fix und kommentare

--- Web-Engineering/ChanceCreate_do.php
+++ Web-Engineering/ChanceCreate_do.php
@@ -15,7 +15,7 @@ require_once("Main/ChanceManager.php");
 require_once("Main/Classes.php");
 
 $description_Chance= htmlspecialchars($_POST["description_Chance"], ENT_QUOTES, "UTF-8");
-$ID_Voting= htmlspecialchars($_POST["ID_Voting"], ENT_QUOTES,"UTF-8");
+$ID_Voting= htmlspecialchars($_GET["ID_Voting"], ENT_QUOTES,"UTF-8");
 
 $user = $_SESSION["user"];
 $userId = $user->ID_User;
