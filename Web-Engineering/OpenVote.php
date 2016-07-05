60336d9ac76fb5826cb623f73ed50507a08a9a11
ea014 <ea014@hdm-stuttgart.de>
2016-07-05 18:40:00 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/OpenVote.php
+++ Web-Engineering/OpenVote.php
@@ -1,16 +1,3 @@
-<html>
-<head>
-    <link type="text/css" rel="stylesheet" href="css/style.css"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
-    <script type="text/javascript" src="js/jquery-1.12.3.js"></script>
-    <script type="text/javascript" src="js/Chart.min.js"></script>
-</head>
-<title>Voting</title>
-<body>
-<div id="navbar">
-    <img src="img/logo2.svg" id="logo">
-</div>
 <?php
 
 // Dieses Skript öffnet das Voting und erzeugt den Teilnahmelink für Studenten.
@@ -48,14 +35,4 @@ echo '<img src="qrCode.php"><br><br>';
 
 // Link abrufen
 echo "Link für Voting: mars.iuk.hdm-stuttgart.de/~mk235/Web-Engineering/Voting.php?ID_Voting=".$ID_Voting;?>
-
-</body>
-
-<footer>
-    <div>
-        <p>© 2016 by Vote.me GmbH - <a href="mailto:support@vote.me">Contact</a> - <a href="impressum.php">Impressum</a></p>
-    </div>
-</footer>
-
-</html>
 
---
+++ Web-Engineering/QR-Code-View.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <script type="text/javascript" src="js/jquery-1.12.3.js"></script>
+    <script type="text/javascript" src="js/Chart.min.js"></script>
+</head>
+<title>Impressum</title>
+<body>
+<div id="navbar">
+    <img src="img/logo2.svg" id="logo">
+</div>
+
+
+<?php
+
+
+
+?>
+
+
+</body>
+
+<footer>
+    <div>
+        <p>© 2016 by Vote.me GmbH - <a href="mailto:support@vote.me">Contact</a> - <a href="impressum.php">Impressum</a></p>
+    </div>
+</footer>
+
+</html>
