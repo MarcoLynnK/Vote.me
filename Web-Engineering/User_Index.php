3e5604f7a64d0f83acbb8a49241f776dd252fb73
ea014 <ea014@hdm-stuttgart.de>
2016-07-05 20:23:19 +0200

    Verlinken, Verbessern, CSS und mehr

--- Web-Engineering/User_Index.php
+++ Web-Engineering/User_Index.php
@@ -20,7 +20,7 @@ require_once("Main/UserManager.php");
 <!--Navigation einbinden-->
 <?php require_once("include/Navbar.php"); ?>
 
-<div class="container">
+<div class="table-container">
     <h1 class="tableText">User</h1>
     <table  class="table table-hover">
         <thead>
@@ -67,7 +67,7 @@ require_once("Main/UserManager.php");
         </tbody>
     </table><br><br>
 </div>
-    
+
 <a href='UserCreate_form.php'><div class='submit'>CREATE USER</div></a>
 </body>
 </html>
