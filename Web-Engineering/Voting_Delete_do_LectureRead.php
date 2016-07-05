b20fa0378ea4b8933d1556474dc27877a126fe80
MarcoLynnK <marco.kagel@googlemail.com>
2016-07-05 16:03:12 +0200

    Lecture_Read erweiterung

--- Web-Engineering/Voting_Delete_do_LectureRead.php
+++ Web-Engineering/Voting_Delete_do_LectureRead.php
@@ -5,14 +5,14 @@
 include ("Main/Session_Check.php");
 require_once("Main/Classes.php");
 require_once("Main/VotingManager.php");
+require_once("Main/LectureManager.php");
 
 $voting = (int)htmlspecialchars($_GET["ID_Voting"], ENT_QUOTES, "UTF-8");
 
 /*
- * Neuer Votingmanager
+ * Neuer Votingmanager für methode Delete
  */
 $votingManager = new VotingManager();
-
 /*
  * Voting anhand des übergebenen Objekts aus Index Löschen
  */
@@ -20,6 +20,27 @@ $votingManager->delete ($voting);
 
 
 /*
+ * Neuer Votingmanager zum auslesen des Objekts Voting, um die LectureID zu finden
+ */
+$votingmanager= new VotingManager();
+
+/*
+ * Methode findById holt das objekt Voting anhand der ID
+ */
+$voting= $votingManager->findById($ID_Voting);
+
+
+/*
+ * neuer LectureManager
+ */
+$lectureManager= new LectureManager();
+/*
+ * Auslesen des Objekts Lecture durch die ID, die in dem Objekt Voting gespeichert ist
+ */
+$lecture= $lectureManager->findById($voting->ID_Lecture);
+
+
+/*
  * Direkte Weiterleitung auf Voting_Index
  */
-header('Location: LectureRead.php');
+header('Location: LectureRead.php?ID_Lecture=$lecture');
